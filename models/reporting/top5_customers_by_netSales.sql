392f567f069ecf89b8393b2dd84ade9ed0cf28c9
Fionn Lim <s10240073@connect.np.edu.sg>
2024-01-19 16:06:48 +0000

    created 2 reports, sales_by_region and top5_customer_by_netSales

---
+++ models/reporting/top5_customers_by_netSales.sql
@@ -0,0 +1,12 @@
+SELECT TOP 5
+  c.customerID as customerID,
+  c.companyName as companyName,
+  SUM(od.netSales) AS netSales
+FROM
+  {{ref('stg_order_detail')}} od
+  INNER JOIN {{ref('raw_orders')}} o ON o.orderID = od.orderID
+  INNER JOIN {{ref('raw_customer')}} c ON c.customerID = o.customerID
+GROUP BY
+    c.customerID, c.companyName
+ORDER BY
+    netSales Desc
