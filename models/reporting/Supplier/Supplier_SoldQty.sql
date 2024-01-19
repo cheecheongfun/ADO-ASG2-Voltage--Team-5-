707b44743cc57a923e6913834c8af9d8121c0052
Fionn Lim <s10240073@connect.np.edu.sg>
2024-01-19 19:11:37 +0000

    organise

--- models/reporting/Supplier/Supplier_SoldQty.sql
+++
@@ -1,6 +0,0 @@
-select companyName , s.supplierid , p.productName , p.productID , SUM(od.quantity) AS totalSales
-from {{ref('raw_supplier')}} s
-INNER JOIN {{ref('raw_product')}} p ON p.supplierid = s.supplierid
-INNER JOIN {{ref('raw_order_details')}} od  ON od.productID = p.productID
-group by companyName,s.supplierid , p.productID , p.productName
-ORDER BY totalSales DESC
