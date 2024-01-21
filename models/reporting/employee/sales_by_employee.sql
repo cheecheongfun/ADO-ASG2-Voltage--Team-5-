80b40919a47d6fc4207853ab3a6114ef3b660ae9
Bryan Lim <s10244231@connect.np.edu.sg>
2024-01-21 09:02:36 +0000

    test3

--- models/reporting/employee/sales_by_employee.sql
+++ models/reporting/employee/sales_by_employee.sql
@@ -1,14 +1,14 @@
 SELECT 
-  se.employeeID as employeeID
-  se.firstname as firstname
-  se.hiredate as hiredate
-  t.territoryID as territoryID
-  t.regionID as regionID
-  sed.discount as discount
-  sed.netSales as netSales
-  sed.quantity as quantity
-  sed.revenue as revenue
-  sed.Unitprice as Unitprice
+  se.employeeID as employeeID,
+  se.firstname as firstname,
+  se.hiredate as hiredate,
+  t.territoryID as territoryID,
+  t.regionID as regionID,
+  sed.discount as discount,
+  sed.netSales as netSales,
+  sed.quantity as quantity,
+  sed.revenue as revenue,
+  sed.Unitprice as Unitprice,
   se.closed_orders_count as closed_orders_count
   from {{ ref ('stg_employee')}} se 
   inner join {{ ref ('raw_territory')}} t
@@ -18,5 +18,3 @@ SELECT
   inner join {{ ref ('stg_order_detail')}} sed 
   on o.orderID inner join sed.orderID
   group by se.employeeID
-
-
