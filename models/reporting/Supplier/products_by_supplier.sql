74257bb421ac1cc7de17bd09c06291781ef95b34
Klodia Tay <s10221043@connect.np.edu.sg>
2024-01-22 13:22:05 +0000

    edits to two files under supplier

--- models/reporting/Supplier/products_by_supplier.sql
+++ models/reporting/Supplier/products_by_supplier.sql
@@ -1,10 +1,11 @@
 SELECT 
     s.companyName,
+    p.productID,
     COUNT(p.productID) AS NumberOfProducts
 FROM 
     {{ ref('raw_supplier') }} s
 INNER JOIN 
     {{ ref('raw_product') }} p ON s.supplierID = p.supplierID
 GROUP BY 
-    s.companyName
+    s.companyName, p.productID
 Order BY NumberOfProducts DESC
