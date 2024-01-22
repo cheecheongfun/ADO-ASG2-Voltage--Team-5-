c52ae9e885c22e18ac9532eec8fa00314cce7a31
Klodia Tay <s10221043@connect.np.edu.sg>
2024-01-22 06:48:44 +0000

    edits to stg_shipment_status.sql

--- models/staging/stg_shipment_status.sql
+++ models/staging/stg_shipment_status.sql
@@ -1,11 +1,9 @@
 {{ config(materialized='view') }}
 
-SELECT 
+SELECT DISTINCT 
     orderID,
     c.companyName AS Customer,
     freight AS Cost,
-    RequiredDate,
-    ShippedDate,
     ShipCountry,
     s.CompanyName,
     CASE WHEN RequiredDate < ShippedDate THEN 'Late' ELSE 'On-time' END AS Status,
@@ -13,5 +11,5 @@ SELECT
     CASE WHEN DATEDIFF(days, ShippedDate, RequiredDate) < 7 THEN 'Red' ELSE 'Green' END AS DeliveryStatus
 FROM 
     {{ ref('raw_orders') }} o 
-INNER JOIN {{ref('raw_shipper')}} s on o.ShipVia = s.shipperID
-INNER JOIN {{ref ('raw_customer')}} c on c.customerID = o.customerID
+INNER JOIN {{ref('raw_shipper')}} s ON o.ShipVia = s.shipperID
+INNER JOIN {{ref ('raw_customer')}} c ON c.customerID = o.customerID
