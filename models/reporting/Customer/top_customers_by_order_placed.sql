92366a9ece9829168d365bf5e1d02ab1bb097b7d
Fionn Lim <fionnlimsj@gmail.com>
2024-01-24 18:18:52 +0000

    add orderDate to customer reporting

--- models/reporting/Customer/top_customers_by_order_placed.sql
+++ models/reporting/Customer/top_customers_by_order_placed.sql
@@ -1,11 +1,12 @@
 SELECT
   c.customerID as customerID,
   c.companyName as companyName,
+  o.orderDate as orderDate,
   SUM(o.orderID) AS total_orders
 FROM
   {{ref('stg_customer')}} c
   INNER JOIN {{ref('raw_orders')}} o ON c.customerID = o.customerID
 GROUP BY
-    c.customerID, c.companyName
+    c.customerID, c.companyName, o.orderDate
 ORDER BY
     total_orders Desc
