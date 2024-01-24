3f364c8fca789f2ac97785d7f8c33672ae6eb4bf
Chun Fai Cheong <cheongchunfai@outlook.com>
2024-01-24 09:51:11 +0000

    supplierDetails edit

--- models/reporting/Supplier/supplierDetails.sql
+++ models/reporting/Supplier/supplierDetails.sql
@@ -1,10 +1,11 @@
 SELECT companyname, 
+       country,
        supplierID,
        sum(TotalQtySold) AS QuantitySold,
        sum(Revenue) AS netSales
 FROM 
     {{ref('stg_supplier')}} 
 GROUP BY 
-    companyname, supplierID
+    companyname, supplierID, country
 ORDER BY 
     QuantitySold DESC
