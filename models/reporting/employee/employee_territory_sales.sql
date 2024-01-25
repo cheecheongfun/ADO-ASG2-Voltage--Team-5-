b20dac8ca28c64ed679b8d8d911d1eee8fde167b
hello.12345.yay@gmail.com <hello.12345.yay@gmail.com>
2024-01-25 03:27:06 +0000

    i

--- models/reporting/employee/employee_territory_sales.sql
+++ models/reporting/employee/employee_territory_sales.sql
@@ -1,11 +1,11 @@
 SELECT 
   t.territoryID,
   e.employeeID,
-  SUM(sod.netSales) as netSales
+  sod.netSales
 FROM
   {{ ref('raw_territory')}} t 
   INNER JOIN {{ref('raw_employee_territory')}} et ON t.territoryID = et.territoryID
   INNER JOIN {{ref('raw_employee')}} e ON et.employeeID = e.employeeID
   INNER JOIN {{ref('raw_orders')}} o ON o.employeeID = e.employeeID 
   INNER JOIN {{ref('stg_order_detail')}} sod ON sod.orderID = sod.orderID
-GROUP BY t.territoryID, e.employeeID
+GROUP BY t.territoryID, e.employeeID, sod.netSales
