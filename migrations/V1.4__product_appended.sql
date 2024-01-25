4c5783c7e29b9ea97251a67b24cdd2080e2a5e89
Chun Fai Cheong <cheongchunfai@outlook.com>
2024-01-25 21:29:55 +0000

    its test 26 now

---
+++ migrations/V1.4__product_appended.sql
@@ -0,0 +1,63 @@
+-- Create or replace the temporary table
+CREATE OR REPLACE TEMPORARY TABLE temp_product (
+    PRODUCTID NUMBER(38,0),
+    PRODUCTNAME VARCHAR(40),
+    LISTPRICE FLOAT,
+    UNITCOST FLOAT,
+    DISCONTINUED VARCHAR(3),
+    CATEGORYNAME VARCHAR(15),
+    SUPPLIER VARCHAR(30),
+    UNIQUE (PRODUCTID)
+);
+
+-- Insert data into the temporary table using FRESH materialization
+INSERT INTO temp_product
+SELECT
+    'Product ID' AS PRODUCTID,
+    'Product Name' AS PRODUCTNAME,
+    'ListPrice' AS LISTPRICE,
+    'UnitCost' AS UNITCOST,
+    'Discontinued' AS DISCONTINUED,
+    'CategoryName' AS CATEGORYNAME,
+    'Supplier' AS SUPPLIER
+FROM @~/migrations/product_fresh.csv;
+
+
+-- Drop columns from PRODUCT table
+ALTER TABLE PRODUCT
+DROP COLUMN IF EXISTS
+    SUPPLIERID,
+    CATEGORYID,
+    QUANTITYPERUNIT,
+    UNITSINSTOCK,
+    UNITSONORDER,
+    REORDERLEVEL,
+    DISCONTINUED;
+
+-- Rename UNITPRICE to LISTPRICE in PRODUCT table
+ALTER TABLE PRODUCT
+RENAME COLUMN UNITPRICE TO LISTPRICE;
+
+-- Add columns to PRODUCT table
+ALTER TABLE PRODUCT
+ADD COLUMN IF NOT EXISTS
+    UNITCOST FLOAT,
+    DISCONTINUED VARCHAR(3),
+    CATEGORYNAME VARCHAR(15),
+    SUPPLIER VARCHAR(30);
+
+-- Update data in PRODUCT table from temp_product
+UPDATE PRODUCT
+SET
+    PRODUCT.PRODUCTNAME = temp_product.PRODUCTNAME
+FROM
+    temp_product
+WHERE
+    PRODUCT.PRODUCTID = temp_product.PRODUCTID;
+
+
+INSERT INTO PRODUCT(PRODUCTID, UNITCOST, DISCONTINUED, CATEGORYNAME, SUPPLIER)
+SELECT
+    temp_product.PRODUCTID,temp_product.UNITCOST,temp_product.DISCONTINUED,temp_product.CATEGORYNAME,temp_product.SUPPLIER
+FROM
+    temp_product;
