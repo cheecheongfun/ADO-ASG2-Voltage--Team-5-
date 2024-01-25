ed5c6229f8016b5c94ebe474b1b42a65e5891f9a
Chun Fai Cheong <cheongchunfai@outlook.com>
2024-01-25 21:24:43 +0000

    test what again huh?

--- migrations/V1.3__product_appended.sql
+++ migrations/V1.3__product_appended.sql
@@ -13,13 +13,13 @@ CREATE OR REPLACE TEMPORARY TABLE temp_product (
 -- Insert data into the temporary table using FRESH materialization
 INSERT INTO temp_product
 SELECT
-    "Product ID" AS PRODUCTID,
-    "Product Name" AS PRODUCTNAME,
-    "ListPrice" AS LISTPRICE,
-    "UnitCost" AS UNITCOST,
-    "Discontinued" AS DISCONTINUED,
-    "CategoryName" AS CATEGORYNAME,
-    "Supplier" AS SUPPLIER
+    'Product ID' AS PRODUCTID,
+    'Product Name' AS PRODUCTNAME,
+    'ListPrice' AS LISTPRICE,
+    'UnitCost' AS UNITCOST,
+    'Discontinued' AS DISCONTINUED,
+    'CategoryName' AS CATEGORYNAME,
+    'Supplier' AS SUPPLIER
 FROM @~/migrations/product_fresh.csv;
 
 
@@ -58,3 +58,10 @@ FROM
     temp_product
 WHERE
     PRODUCT.PRODUCTID = temp_product.PRODUCTID;
+
+
+INSERT INTO PRODUCT(PRODUCTID, UNITCOST, DISCONTINUED, CATEGORYNAME, SUPPLIER)
+SELECT
+    temp_product.PRODUCTID,temp_product.UNITCOST,temp_product.DISCONTINUED,temp_product.CATEGORYNAME,temp_product.SUPPLIER
+FROM
+    temp_product;
