97155e0604cc9f6595fe14729d9824f0f5e38304
Fionn Lim <fionnlimsj@gmail.com>
2024-01-27 12:41:02 +0000

    adjustment to demographic and top customers for order count

--- models/reporting/Customer/customer_demographic.sql
+++ models/reporting/Customer/customer_demographic.sql
@@ -1,17 +1,20 @@
 SELECT
-  c.customerID AS customerID,
-  c.companyName AS companyName,
-  c.contactTitle AS contactTitle,
-  p.categoryName AS categoryName,
-  ROUND(SUM(od.netSales), 1) AS TotalSales,
-  COUNT(o.orderId) AS TotalOrders,
-  SUM(od.profit) AS Profit
+  c.customerID as customerID,
+  c.companyName as companyName,
+  o.orderDate as orderDate,
+  o.orderDay as orderDay,
+  o.orderMonth AS orderMonth,
+  o.orderYear AS orderYear,
+  COUNT(o.orderID) AS total_orders,
+  SUM(od.netSales) AS TotalSales,
+  SUM(od.unitCost * od.quantity) AS TotalCost,
+  ROUND(SUM(od.profit), 1) AS Profit,
+  AVG(ProfitMargin) AS ProfitMargin
 FROM
-  {{ ref('stg_customer') }} c
-  INNER JOIN {{ ref('stg_orders') }} o ON c.customerID = o.customerID
-  INNER JOIN {{ ref('stg_order_detail') }} od ON od.orderID = o.orderID
-  INNER JOIN {{ ref('raw_product') }} p ON p.productID = od.productID
+  {{ref('stg_customer')}} c
+  INNER JOIN {{ref('stg_orders')}} o ON c.customerID = o.customerID
+  INNER JOIN {{ref("stg_order_detail")}} od ON o.orderid = od.orderid
 GROUP BY
-  c.customerID, c.companyName, c.contactTitle, p.categoryName
+    c.customerID, c.companyName, o.orderDate, o.orderDay, o.orderMonth, o.orderYear
 ORDER BY
-  c.customerID
+    total_orders Desc
