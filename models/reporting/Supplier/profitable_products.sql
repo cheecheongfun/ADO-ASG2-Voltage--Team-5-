1324a3fbd4c9eb6bfd0d5146e72cb19587f4409d
klodia tay <klodeeuh@gmail.com>
2024-01-27 13:45:59 +0000

    reports edits

--- models/reporting/Supplier/profitable_products.sql
+++ models/reporting/Supplier/profitable_products.sql
@@ -6,12 +6,13 @@ SELECT
     SUM(od.quantity) AS qty,
     SUM(od.Unitprice * od.quantity) AS Revenue,
     SUM(od.profit) AS Profit,
-    discontinued
+    discontinued,
+    profitMargin
 FROM {{ref ('stg_order_detail')}} od 
 INNER JOIN {{ ref ('raw_product')}} p ON od.productID = p.productID
 WHERE 
     p.discontinued != 'Yes'
 GROUP BY 
-    od.orderid, od.productid, p.productname,P.DISCONTINUED,p.categoryName
+    od.orderid, od.productid, p.productname,P.DISCONTINUED,p.categoryName,profitMargin
 HAVING 
     SUM(od.profit) >= 0.5 * SUM(Revenue)
