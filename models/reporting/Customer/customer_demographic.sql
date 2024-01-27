cc44cc95c99ce1039b84d99bdc85554cc9a19be1
Fionn Lim <fionnlimsj@gmail.com>
2024-01-27 04:12:53 +0000

    remove round

--- models/reporting/Customer/customer_demographic.sql
+++ models/reporting/Customer/customer_demographic.sql
@@ -1,15 +1,17 @@
 SELECT
-  c.customerID as customerID, 
-  c.companyName as companyName,
-  c.contactTitle as contactTitle, 
-  p.categoryName as categoryName, 
-  ROUND(SUM(od.netSales), 1) as TotalSales,
-  ROUND(SUM(od.profit), 1) as Profit,
-  ROUND(AVG(od.profitMargin)) as ProfitMargin
+  c.customerID AS customerID,
+  c.companyName AS companyName,
+  c.contactTitle AS contactTitle,
+  p.categoryName AS categoryName,
+  ROUND(SUM(od.netSales), 1) AS TotalSales,
+  ROUND(SUM(od.profit), 1) AS Profit,
+  AVG(ProfitMargin) AS ProfitMargin
 FROM
-  {{ref('stg_customer')}} c
-  INNER JOIN {{ref('stg_orders')}} o ON c.customerID = o.customerID
-  INNER JOIN {{ref('stg_order_detail')}} od ON od.orderID = o.orderID
-  INNER JOIN {{ref('raw_product')}} p ON p.productID = od.productID
+  {{ ref('stg_customer') }} c
+  INNER JOIN {{ ref('stg_orders') }} o ON c.customerID = o.customerID
+  INNER JOIN {{ ref('stg_order_detail') }} od ON od.orderID = o.orderID
+  INNER JOIN {{ ref('raw_product') }} p ON p.productID = od.productID
 GROUP BY
-    c.customerID, c.companyName, c.contactTitle, p.categoryName
+  c.customerID, c.companyName, c.contactTitle, p.categoryName
+ORDER BY
+  c.customerID
