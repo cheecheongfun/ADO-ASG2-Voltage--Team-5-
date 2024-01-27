cbf9a60cdbf0b743737a1b0f7e872821963a7345
Fionn Lim <fionnlimsj@gmail.com>
2024-01-27 13:57:52 +0000

    added TBC (to be confirmed) status for shipping

--- models/staging/stg_shipment_status.sql
+++ models/staging/stg_shipment_status.sql
@@ -10,12 +10,21 @@ SELECT DISTINCT
     OrderDate,
     s.CompanyName,
     RequiredDate,
-    CASE WHEN RequiredDate < ShippedDate THEN 'Late' ELSE 'On-time' END AS Status,
-    DATEDIFF(days, OrderDate, ShippedDate) AS ProccesingTime,
-    CASE WHEN DATEDIFF(days, ShippedDate, RequiredDate) < 7 THEN 1 ELSE 0 END AS Red,
-    CASE WHEN DATEDIFF(days, ShippedDate, RequiredDate) > 7 THEN 1 ELSE 0 END AS Green
-
+    CASE 
+        WHEN ShippedDate IS NULL THEN 'TBC' 
+        WHEN RequiredDate < ShippedDate THEN 'Late' 
+        ELSE 'On-time' 
+    END AS Status,
+    COALESCE(DATEDIFF(days, OrderDate, ShippedDate), 0) AS ProcessingTime,
+    CASE 
+        WHEN DATEDIFF(days, ShippedDate, RequiredDate) < 7 THEN 1 
+        ELSE 0 
+    END AS Red,
+    CASE 
+        WHEN DATEDIFF(days, ShippedDate, RequiredDate) > 7 THEN 1 
+        ELSE 0 
+    END AS Green
 FROM 
     {{ ref('stg_orders') }} o 
-INNER JOIN {{ref('raw_shipper')}} s ON o.ShipVia = s.shipperID
-INNER JOIN {{ref ('raw_customer')}} c ON c.customerID = o.customerID
+    INNER JOIN {{ ref('raw_shipper') }} s ON o.ShipVia = s.shipperID
+    INNER JOIN {{ ref('raw_customer') }} c ON c.customerID = o.customerID
