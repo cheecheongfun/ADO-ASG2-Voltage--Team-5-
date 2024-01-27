6a37890b0ef500174acb57c4cd3b4350bf59337f
Fionn Lim <fionnlimsj@gmail.com>
2024-01-27 05:30:54 +0000

    added date table for powerbi connection

--- models/reporting/Customer/top_customers_by_order_placed.sql
+++ models/reporting/Customer/top_customers_by_order_placed.sql
@@ -5,10 +5,15 @@ SELECT
   o.orderDay as orderDay,
   o.orderMonth AS orderMonth,
   o.orderYear AS orderYear,
-  SUM(o.orderID) AS total_orders
+  COUNT(o.orderID) AS total_orders,
+  SUM(od.netSales) AS TotalSales,
+  SUM(od.unitCost * od.quantity) AS TotalCost,
+  ROUND(SUM(od.profit), 1) AS Profit,
+  AVG(ProfitMargin) AS ProfitMargin
 FROM
   {{ref('stg_customer')}} c
   INNER JOIN {{ref('stg_orders')}} o ON c.customerID = o.customerID
+  INNER JOIN {{ref("stg_order_detail")}} od ON o.orderid = od.orderid
 GROUP BY
     c.customerID, c.companyName, o.orderDate, o.orderDay, o.orderMonth, o.orderYear
 ORDER BY
